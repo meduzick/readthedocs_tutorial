d55508d11431066d6c0083b15cf2a54e5c072336
Nikita Martynov <nikitamartynov@MacBook-Air-od-Nikita.local>
2024-02-16 16:55:44 +0300

    adf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,20 @@ html_static_path = ['images']
 html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
-    'navigation_depth': 10,
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
 
 # -- Options for EPUB output
